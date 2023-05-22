aeef844878c4d1719b0e8920eccb24dbd52119f6
Hassan EL BAHI <me_80_me@hotmail.com>
2023-05-22 23:06:49 +0100

    Message changning acordding to time!

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -3,10 +3,23 @@ const today = require('./today');
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end(`Hello, World! The date today is ${today.getDate()}`);
+  let date = new Date();
+  let hour = date.getHours(date);
+  let message = '';
+
+  if (hour >= 0 && hour < 12 ){
+      message = 'Good morning!';
+  } else if ( hour >= 12 && hour > 18) {
+      message = 'Good afternoon!'
+  } else if ( hour >= 18 && hour < 21){
+      message = 'Good evening!'
+  } else {
+      message = 'Good night!'
+  }
+  res.end(`Hello,  ${message}: ${today.getDate()}`);
 }
 
-const port = 8080;
+const port = 8082;
 const server = http.createServer(requestListener);
 console.log('server listening on port: ' + port);
 server.listen(port);
